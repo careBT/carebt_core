b42f227331567316de5fe766a180c17ae72dbf6a
Andreas Steck <steck.andi@gmail.com>
2021-10-04 23:18:59 +0200

    code-coverage with --branch

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-coverage run --source=carebt -m pytest --cache-clear --flake8 -s -vv && coverage html
+coverage run --branch --source=carebt -m pytest --cache-clear --flake8 -s -vv && coverage html
