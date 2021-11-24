cd0bd3f38b5c1d28e56ebfc31f78ae11e7274dcc
Andreas Steck <steck.andi@gmail.com>
2021-11-24 22:16:53 +0100

    tests added

--- tests/examples/test_action_with_params.py
+++ tests/examples/test_action_with_params.py
@@ -18,6 +18,7 @@ import re
 
 from unittest.mock import patch
 
+from carebt.abstractLogger import LogLevel
 from carebt.behaviorTreeRunner import BehaviorTreeRunner
 from carebt.examples.action_with_params import AddTwoNumbersAction
 from carebt.nodeStatus import NodeStatus
@@ -26,10 +27,30 @@ from carebt.nodeStatus import NodeStatus
 class TestActionWithParams:
 
     @patch('sys.stdout', new_callable=StringIO)
-    def test_AddTwoNumbersAction(self, mock_print):
+    def test_AddTwoNumbersAction_ok(self, mock_print):
         bt_runner = BehaviorTreeRunner()
         bt_runner.run(AddTwoNumbersAction, '1 2 => ?x')
         regex = re.compile(r'AddTwoNumbersAction: calculating: 1 \+ 2 = 3\n')
+        assert bool(re.match(regex, mock_print.getvalue()))
+        assert bt_runner.get_status() == NodeStatus.SUCCESS
+        assert bt_runner.get_contingency_message() == ''
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_AddTwoNumbersAction_one_missing(self, mock_print):
+        bt_runner = BehaviorTreeRunner()
+        bt_runner.get_logger().set_log_level(LogLevel.OFF)
+        bt_runner.run(AddTwoNumbersAction, '1 => ?x')
+        regex = re.compile(r'AddTwoNumbersAction: calculating: 1 \+ 0 = 1\n')
+        assert bool(re.match(regex, mock_print.getvalue()))
+        assert bt_runner.get_status() == NodeStatus.SUCCESS
+        assert bt_runner.get_contingency_message() == ''
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_AddTwoNumbersAction_both_missing(self, mock_print):
+        bt_runner = BehaviorTreeRunner()
+        bt_runner.get_logger().set_log_level(LogLevel.OFF)
+        bt_runner.run(AddTwoNumbersAction, '=> ?x')
+        regex = re.compile(r'AddTwoNumbersAction: calculating: 0 \+ 0 = 0\n')
         assert bool(re.match(regex, mock_print.getvalue()))
         assert bt_runner.get_status() == NodeStatus.SUCCESS
         assert bt_runner.get_contingency_message() == ''
