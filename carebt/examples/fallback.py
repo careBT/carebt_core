87b206703afcf36a398cd936737696ebbcc93dc3
Andreas Steck <steck.andi@gmail.com>
2021-11-30 22:18:05 +0100

    fallback example added

---
+++ carebt/examples/fallback.py
@@ -0,0 +1,47 @@
+# Copyright 2021 Andreas Steck (steck.andi@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from carebt.fallbackNode import FallbackNode
+
+from carebt.examples.sequence_with_contingencies import AddTwoNumbersActionWithFailures
+
+
+class SimpleFallback(FallbackNode):
+    """
+    The `SimpleFallback` has three input parameters and contains three child
+    nodes of the same type (`AddTwoNumbersActionWithFailures`). Each of the child
+    nodes has the first input parameter fixed and the second one is taken from the
+    input of the `SimpleFallback`. This structure allows to provoke a failure in
+    each of the child nodes, for example, by setting the input (*?b1 ?b2 ?b3*) to
+    a number that the sum of this value and the fixed one is greater than ten.
+
+
+    Input Parameters
+    ----------------
+    ?b1 : int
+        The first value
+    ?b2 : int
+        The second value
+    ?b3 : int
+        The second value
+
+    """
+
+    def __init__(self, bt_runner):
+        super().__init__(bt_runner, '?b1 ?b2 ?b3')
+
+    def on_init(self) -> None:
+        self.append_child(AddTwoNumbersActionWithFailures, '1 ?b1 => ?c1')
+        self.append_child(AddTwoNumbersActionWithFailures, '2 ?b2 => ?c2')
+        self.append_child(AddTwoNumbersActionWithFailures, '3 ?b3 => ?c3')
---
+++ tests/examples/test_fallback.py
@@ -0,0 +1,67 @@
+# Copyright 2021 Andreas Steck (steck.andi@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from io import StringIO
+
+import re
+
+from unittest.mock import patch
+
+from carebt.behaviorTreeRunner import BehaviorTreeRunner
+from carebt.examples.fallback import SimpleFallback
+from carebt.nodeStatus import NodeStatus
+
+
+class TestFallback:
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_FallbackNode_1(self, mock_print):
+        bt_runner = BehaviorTreeRunner()
+        bt_runner.run(SimpleFallback, '1 2 3')
+        assert bt_runner.get_status() == NodeStatus.SUCCESS
+        assert bt_runner.get_contingency_message() == ''
+        regex = re.compile(r'AddTwoNumbersActionWithFailures: calculating: 1 \+ 1 = 2\n')
+        assert bool(re.match(regex, mock_print.getvalue()))
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_FallbackNode_2(self, mock_print):
+        bt_runner = BehaviorTreeRunner()
+        bt_runner.run(SimpleFallback, '10 2 3')
+        assert bt_runner.get_status() == NodeStatus.SUCCESS
+        assert bt_runner.get_contingency_message() == ''
+        regex = re.compile(r'AddTwoNumbersActionWithFailures: calculating: 1 \+ 10 = 11 -> RESULT_TOO_LARGE\n'  # noqa: E501
+                           r'AddTwoNumbersActionWithFailures: calculating: 2 \+ 2 = 4\n')
+        assert bool(re.match(regex, mock_print.getvalue()))
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_FallbackNode_3(self, mock_print):
+        bt_runner = BehaviorTreeRunner()
+        bt_runner.run(SimpleFallback, '10 20 3')
+        assert bt_runner.get_status() == NodeStatus.SUCCESS
+        assert bt_runner.get_contingency_message() == ''
+        regex = re.compile(r'AddTwoNumbersActionWithFailures: calculating: 1 \+ 10 = 11 -> RESULT_TOO_LARGE\n'  # noqa: E501
+                           r'AddTwoNumbersActionWithFailures: calculating: 2 \+ 20 = 22 -> RESULT_TOO_LARGE\n'  # noqa: E501
+                           r'AddTwoNumbersActionWithFailures: calculating: 3 \+ 3 = 6\n')
+        assert bool(re.match(regex, mock_print.getvalue()))
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_FallbackNode_4(self, mock_print):
+        bt_runner = BehaviorTreeRunner()
+        bt_runner.run(SimpleFallback, '10 20 30')
+        assert bt_runner.get_status() == NodeStatus.FAILURE
+        assert bt_runner.get_contingency_message() == 'RESULT_TOO_LARGE'
+        regex = re.compile(r'AddTwoNumbersActionWithFailures: calculating: 1 \+ 10 = 11 -> RESULT_TOO_LARGE\n'  # noqa: E501
+                           r'AddTwoNumbersActionWithFailures: calculating: 2 \+ 20 = 22 -> RESULT_TOO_LARGE\n'  # noqa: E501
+                           r'AddTwoNumbersActionWithFailures: calculating: 3 \+ 30 = 33 -> RESULT_TOO_LARGE\n')  # noqa: E501
+        assert bool(re.match(regex, mock_print.getvalue()))
