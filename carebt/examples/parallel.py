fb28d8a9688af3642e0ae2d8853904634897b697
Andreas Steck <steck.andi@gmail.com>
2021-11-30 22:18:15 +0100

    parallel example added

---
+++ carebt/examples/parallel.py
@@ -0,0 +1,47 @@
+# Copyright 2021 Andreas Steck (steck.andi@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from carebt.parallelNode import ParallelNode
+
+from carebt.examples.longrun_actions import AddTwoNumbersMultiTickAction
+
+
+class SimpleParallel(ParallelNode):
+    """
+    The `SimpleParallel` node has three child nodes of the same type
+    (`AddTwoNumbersMultiTickAction`). With the three input parameters it can be
+    controlled how many tick each of the nodes take to complete. The
+    `success_threshold` of the `SimpleParallel` node is set to two. That means,
+    that the whole node succeeds as soon as two nodes have completed with
+    `SUCCESS` or `FIXED` or fails as soon as one of the child nodes complete
+    with `FAILURE` or `ABORTED`.
+
+    Input Parameters
+    ----------------
+    ?ticks1 : int
+        The ticks for the first child
+    ?ticks2 : int
+        The ticks for the second child
+    ?ticks3 : int
+        The ticks for the third child
+
+    """
+
+    def __init__(self, bt_runner):
+        super().__init__(bt_runner, 2, '?ticks1 ?ticks2 ?ticks3')
+
+    def on_init(self) -> None:
+        self.add_child(AddTwoNumbersMultiTickAction, '?ticks1 1 1 => ?c1')
+        self.add_child(AddTwoNumbersMultiTickAction, '?ticks2 2 2 => ?c2')
+        self.add_child(AddTwoNumbersMultiTickAction, '?ticks3 3 3 => ?c3')
